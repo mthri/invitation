adab41d5c9412fe865d0c3e503ad140eb96f33c1
Amir Motahari <amir.motahari@outlook.com>
2021-04-23 03:15:50 +0430

    create structure of project

---
+++ utils/config.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+
+import yaml
+
+def _get_config():
+    BASE_DIR = str(Path(__file__).resolve().parent.parent)
+    with open(os.path.join(BASE_DIR, 'config.yml')) as f:
+        config = yaml.safe_load(f,)
+    config['BASE_DIR'] = BASE_DIR
+
+
+    if os.path.exists(config['INVITION_CONFIG'].replace('BASE_DIR', BASE_DIR)):
+        with open(config['INVITION_CONFIG'].replace('BASE_DIR', BASE_DIR)) as f:
+            config.update(yaml.load(f, yaml.SafeLoader) or {})
+
+
+CONFIG = _get_config()
