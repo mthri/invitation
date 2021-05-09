cd351a2e9b49bd03519f9bd2b552091dd8442af7
Amir Motahari <amir.motahari@outlook.com>
2021-05-10 03:57:23 +0430

    add custom login req mixin

---
+++ dashboard/mixins.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.mixins import AccessMixin
+
+class PremissionMixin(AccessMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
