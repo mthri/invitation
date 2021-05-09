f4b04e67ab0928c07cf7e09986070c2f4bf67cf0
Amir Motahari <amir.motahari@outlook.com>
2021-05-10 03:57:57 +0430

    finish edit user info by own

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/', views.Profile.as_view(), name='profile'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# @method_decorator(login_required, name='dispatch')
+from django.views.generic import View
+
+from .mixins import PremissionMixin
+from .forms import UserForm
+
 @login_required
 def index(request):
     return render(request, 'dashboard/base.html')
+    
+class Profile(PremissionMixin, View):
 
-def profile(request):
-    return render(request, 'dashboard/profile.html', context={'user': request.user})
+    def get(self, request, *args, **kwargs):
+        return render(request, 'dashboard/profile.html', 
+                      context={'user': request.user})
+    
+    def post(self, request, *args, **kwargs):
+        form = UserForm(request.POST, instance=self.request.user)
+        if form.is_valid():
+            form.save()
+
+        return render(request, 'dashboard/profile.html', 
+                      context={'user': request.user,
+                               'errors': form.errors.get_json_data()})
