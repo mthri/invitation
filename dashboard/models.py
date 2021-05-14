06b007ae9aaefabc0ffef258d63f9104c882a7ef
Amir Motahari <amir.motahari@outlook.com>
2021-05-14 20:28:28 +0430

    create tag model

--- dashboard/models.py
+++ dashboard/models.py
@@ -21,8 +21,26 @@ class Contact(Model):
                               verbose_name=_('مالک'), related_name='contacts', 
                               null=True, db_index=True)
     created_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=_('تاریخ ایجاد'))
-    communicative_road = JSONField(verbose_name=('راه های ارتباطی'))
+    communicative_road = models.JSONField(verbose_name=('راه های ارتباطی'))
     tags = SetCharField(base_field=models.IntegerField(), size=10, max_length=(10*3), 
                         verbose_name=_('تگ'), null=True, blank=True)
     is_deleted = models.BooleanField(default=False, verbose_name=_('آیا حذف شده است؟'))
 
+    def __str__(self) -> str:
+        return self.first_name + ' ' + self.last_name
+
+class Tag(Model):
+    class Meta:
+        verbose_name = _('برچسب')
+        verbose_name_plural = _('برچسب ها')
+
+    created_at = models.DateTimeField(auto_now=True, verbose_name=_('تاریخ ایجاد'))
+    owner = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, 
+                              verbose_name=_('مالک'), related_name='tags', 
+                              null=True, db_index=True)
+    name = models.CharField(max_length=20, verbose_name=_('نام'), default=_('ندارد'))
+    description = models.CharField(max_length=100, verbose_name=_('توضیح'), null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+       
