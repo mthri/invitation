309897537d209b31ba7ce2a35dd0eef2ef95fdb6
Amir Motahari <amir.motahari@outlook.com>
2021-05-19 03:59:40 +0430

    add datatable base view with capability serach and pagging

---
+++ utils/generic_view.py
@@ -0,0 +1,46 @@
+from django.views.generic import View
+from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
+
+from utils.response import SuccessJsonResponse, BadJsonResponse
+
+class SetToListJSONEncoder(DjangoJSONEncoder):
+     def default(self, o):
+        if isinstance(o, set):
+            return list(o)
+        else:
+            return super().default(o)
+
+
+class DataTableView(View):
+    http_method_names = ['post', 'options']
+    max_length = 100
+    query_set = None
+    result_args = None
+    result_kwargs = None
+    search_on_field = None
+
+    def post(self, request, *args, **kwargs):
+        count = self.query_set.count()
+
+        # get data from client
+        start = int(request.POST.get('start', 0))
+        length = int(request.POST.get('length', 0))
+        search = request.POST.get('search', None)
+
+        if search and search.strip() != '':
+            search_field = { self.search_on_field+'__icontains': search }
+            self.query_set = self.query_set.filter(**search_field)
+
+        # we can't set length  greater than max_length
+        if length > self.max_length:
+            length = self.max_length
+
+        # limit our result
+        self.query_set = self.query_set[start:(start+length)]
+
+        results = list(self.query_set.values(*self.result_args, **self.result_kwargs))
+
+        return SuccessJsonResponse({'data': results, 'iTotalDisplayRecords': count, 'iTotalRecords': count}, 
+                                    encoder=SetToListJSONEncoder)
+
