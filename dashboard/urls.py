405e44b66aa6baa6b09ff06367cd8e2b8b18a8ac
Amir Motahari <amir.motahari@outlook.com>
2021-05-23 01:31:52 +0430

    remove unusable css and correcting the url address

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('profile/', views.Profile.as_view(), name='profile'),
     path('contact/', views.Contact.as_view(), name='contact'),
     path('setting/', views.Setting.as_view(), name='setting'),
-    path('invite', views.Invite.as_view(), name='invite'),
+    path('invite/', views.Invite.as_view(), name='invite'),
 ]
 
 # AJAX url
