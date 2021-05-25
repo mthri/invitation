296c721892e8e120d5854b339c637d3be25f1dac
Amir Motahari <amir.motahari@outlook.com>
2021-05-26 02:39:30 +0430

    force data must be dict for `SuccessJsonResponse` and `BadJsonResponse`

--- utils/response.py
+++ utils/response.py
@@ -1,12 +1,15 @@
 from django.http import JsonResponse
 
+
 class SuccessJsonResponse(JsonResponse):
-    def __init__(self, data=dict(), status=200, *args, **kwargs) -> None:
+    def __init__(self, data: dict = dict(), status=200, *args, **kwargs) -> None:
         data['status'] = 'success'
         super().__init__(data, status=status, *args, **kwargs)
 
 # for bad request error code 400
+
+
 class BadJsonResponse(JsonResponse):
-    def __init__(self, data=dict(), status=400, *args, **kwargs) -> None:
+    def __init__(self, data: dict = dict(), status=400, *args, **kwargs) -> None:
         data['status'] = 'error'
         super().__init__(data, status=status, *args, **kwargs)
