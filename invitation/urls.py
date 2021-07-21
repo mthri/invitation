767f1ffe4d31732f5697bf281f926948cd6fe050
Amir Motahari <amir.motahari@outlook.com>
2021-07-22 01:31:13 +0430

    serve media by django on development

--- invitation/urls.py
+++ invitation/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 from dashboard.admin import admin_site
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin_site.urls),
     path('panel/', include('dashboard.urls')),
     path('auth/', include('authentication.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
