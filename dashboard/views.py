3180499e931823eaeede0164c6896aa93ee30d8c
Amir Motahari <amir.motahari@outlook.com>
2021-08-08 22:07:18 +0430

    create contact with file

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,15 @@
-from dashboard.models import Template
+import uuid
+
+from django.http.request import HttpRequest
+from dashboard.models import Template, Tag, Contact as ContactModel
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import View
 
 from .mixins import PremissionMixin
 from .forms import UserForm
+from utils.upload import save_uploaded_file
+from utils.config import CSV_DIRECTORY_PATH, CONFIG
 
 @login_required
 def index(request):
@@ -25,15 +30,48 @@ class Profile(PremissionMixin, View):
                       context={'user': request.user,
                                'errors': form.errors.get_json_data()})
 
+
 class Contact(PremissionMixin, View):
 
     def get(self, request, *arg, **kwargs):
+        return render(request, 'dashboard/contact_manage.html', context={'csv_sample': 'upload/upload/csv/sample.csv'})
+
+    def post(self, request:HttpRequest, *args, **kwargs):
+        path = CONFIG['FILE_UPLOAD_TEMP_DIR'] + '/' + CSV_DIRECTORY_PATH + str(uuid.uuid4())
+        save_uploaded_file(path=path, file=request.FILES['file'])
+
+        tags = [int(tag) for tag in request.POST.getlist('tags')]
+
+        tags = Tag.validate_tags(request.user, tags)
+        contact_list = []
+        with open(path, 'r', encoding="utf8") as f:
+            # skip first row
+            lines = f.readlines()[1:]
+            for line in lines:
+                cols = line.split(',')
+                first_name = cols[0]
+                last_name = cols[1]
+                phone = cols[2]
+                email = cols[3]
+                contact_list.append(ContactModel(
+                    first_name=first_name,
+                    last_name=last_name,
+                    phone=phone,
+                    tags=tags,
+                    owner=request.user,
+                    communicative_road={'email': email}
+                ))
+        
+        ContactModel.objects.bulk_create(contact_list)
+
         return render(request, 'dashboard/contact_manage.html')
+
 
 class Setting(PremissionMixin, View):
     
     def get(self, request, *arg, **kwargs):
         return render(request, 'dashboard/setting.html')
+
 
 class Invite(PremissionMixin, View):
     
@@ -43,15 +81,18 @@ class Invite(PremissionMixin, View):
         }
         return render(request, 'dashboard/invite.html', context=content)
 
+
 class InviteHistory(PremissionMixin, View):
 
     def get(self, request, *args, **kwargs):
         return render(request, 'dashboard/invite_list.html')
 
+
 class Purchase(PremissionMixin, View):
 
     def get(self, request, *args, **kwargs):
         return render(request, 'dashboard/purchase.html')
+
 
 class Transactions(PremissionMixin, View):
 
--- utils/config.py
+++ utils/config.py
@@ -5,6 +5,7 @@ import yaml
 
 THUMBNAIL_DIRECTORY_PATH = 'thumbnail/'
 TEMPLATE_DIRECTORY_PATH = 'templates/'
+CSV_DIRECTORY_PATH = 'csv/'
 
 def _get_config():
     BASE_DIR = str(Path(__file__).resolve().parent.parent)
