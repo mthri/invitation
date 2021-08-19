727f8263162c4147842620871fa0e26e5e5b05e7
Amir Motahari <amir.motahari@outlook.com>
2021-08-19 19:46:49 +0430

    render string as django template

--- utils/email.py
+++ utils/email.py
@@ -1,5 +1,11 @@
 from django.core.mail import send_mail
+from django.template import Template, Context
 
 # TODO implement this method
 def send_email(email_address:str, subject:str, message:str):
     pass
+
+def render_to_string(template:str, context:dict):
+    template = Template(template)
+    context = Context(context)
+    return template.render(context)
